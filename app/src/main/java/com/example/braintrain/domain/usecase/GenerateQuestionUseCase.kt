af9a0c8e923dcbc0f7b1946d929a4623acd12fdb
Qarapapax <aldunsar@yahoo.com>
2023-01-18 19:55:42 +0300

    4 entities(1 enum, 3 data classes) and 2 usecases added

---
+++ app/src/main/java/com/example/braintrain/domain/usecase/GenerateQuestionUseCase.kt
@@ -0,0 +1,16 @@
+package com.example.braintrain.domain.usecase
+
+import com.example.braintrain.domain.entity.Question
+import com.example.braintrain.domain.repository.GameRepository
+
+class GenerateQuestionUseCase(private val repository: GameRepository) {
+
+    operator fun invoke(maxSumValue: Int): Question {
+        return repository.generateQuestion(maxSumValue, COUNT_OF_OPTIONS)
+    }
+
+    private companion object {
+
+        private const val COUNT_OF_OPTIONS = 6
+    }
+}
